7b068d52ecc2414140bd84f393ef9b9c32667180
Nik-Stevens <nicostiv007@hotmail.com>
2023-08-17 22:39:51 -0500

    Platform
    A Platform that is moves from bottom up

---
+++ Source/Arkde_RoomPuzzle/Public/RP_Platform.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RP_Platform.generated.h"
+
+class UStaticMeshComponent;
+
+UCLASS()
+class ARKDE_ROOMPUZZLE_API ARP_Platform : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USceneComponent* CustomRootComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* PlatformMeshComponent;
+
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Platform")
+	bool bIsGoingUp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Platform")
+	float Speed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Platform")
+	float MinHeight;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Platform")
+	float MaxHeight;
+	
+public:	
+	// Sets default values for this actor's properties
+	ARP_Platform();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Movement")
+	void Move();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
